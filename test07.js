e89b1f7969be7e9c050f3136f15f36b407032cd6
luis364 <llopez@smartech.com.mx>
2019-02-18 12:59:08 -0600

    initial commit

---
+++ test07.js
@@ -0,0 +1,6 @@
+navigator.bluetooth.requestDevice({
+  acceptAllDevices: true,
+  optionalServices: ['battery_service']
+})
+.then(device => { /* ... */ })
+.catch(error => { console.log(error); });
